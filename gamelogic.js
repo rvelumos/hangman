4b40df17009da319f08a23f453348a0a1fc2d024
Ronald <ronald.eijsden@gmail.com>
2021-01-29 17:55:54 +0100

    added functions for complete game

--- gamelogic.js
+++ gamelogic.js
@@ -1,22 +1,59 @@
 function displayWordSoFar(word, guesses) {
-  // - [ ] zet je testje op test.only (later weer weghalen)
-  // - [ ] loggen: wat is word, wat is guesses
-  // - [ ] javascript, ['a', 'b'] -> "_ _ _ _ _ _ _ _ _ _ "
-  // - [ ] javascript "splitten" split() -> ['j','a','v','a','s','c','r','i','p','t']
-  // - [ ] een loop maken -> ['j','a','v','a','s','c','r','i','p','t']
-  // - [ ] checken: zit de letter in guesses?
-  // - [ ] if else
-  // - [ ] zo nee: "_ "
-  // - [ ] zo ja: "j " (niet j, maar de letter die we dan checken)
-  // - [ ] klaar? -> onze string die we hebben gemaakt returnen
+  // WRITE ME
+
+  const letterArray = word.split("");
+  let output = '';
+  for (let i = 0; i < letterArray.length; i++) {
+    const letterInWord = letterArray[i];
+    const letterGuessed = guesses.includes(letterInWord);    
+    if(!letterGuessed){
+        output = output + "_ ";
+    }else{
+        output = output + letterInWord + " ";
+    }
+  }  
+  console.log(output);
+  return(output);
 }
 
 function isGameWon(word, guesses) {
   // WRITE ME
+  const letterArray = word.split("");
+  let createdWord = "";
+
+  for (let i = 0; i < letterArray.length; i++) {
+    const letterInWord = letterArray[i];
+    const letterCorrect = guesses.includes(letterInWord);
+    if (letterCorrect) {
+      createdWord += letterInWord;
+    }
+  }
+
+  if(createdWord===word) {
+      return true;
+  }
+  return false;
 }
 
 function isGameLost(word, guesses) {
   // WRITE ME
+  let lives = 7;
+
+  for (let i = 0; i < guesses.length; i++) {
+    const inputCorrect = word.includes(guesses[i]);
+
+    if (!inputCorrect) {
+        lives--;
+    }
+  }
+
+  console.log("\nje mag nog " + lives + " foute antwoorden geven.\n");
+
+  if(lives === 0) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -7,10 +7,41 @@ function game(word, guesses) {
   const letter = question("Raad een letter: ");
 
   // voeg de geraden letter toe aan de array met guesses
-  guesses.push(letter);
+  if(letter.match(/^[A-Za-z]$/)) {
+    let characterExists;
+    for(let i=0; i<guesses.length;i++){
+      if(guesses.includes(letter)){
+        characterExists=true;
+      }
+    }
+    if(!characterExists) {
+      guesses.push(letter.toLowerCase());
+    }else{
+      console.log("Je hebt dit teken al gebruikt, probeer een ander \n")
+    }
+  }else{
+    console.log("Ongeldige invoer: alleen 1 alfabetische letter is toegestaan \n");
+  }
+  const guessedWord = displayWordSoFar(word, guesses);
 
   // volgende ronde! we roepen game nog een keer aan
-  game(word, guesses);
+
+  if(isGameLost(word, guesses)){
+    console.log(`
+   +---+
+   |   |
+   O   |
+  /|\\ |
+  / \\ |
+       |
+  =========
+`);
+      console.log("\n Helaas, je hebt verloren");
+  }else if(isGameWon(word, guesses)) {
+      console.log("Je hebt gewonnen!");
+  }else{
+      game(word, guesses);
+  }
 }
 
 console.log(`
@@ -24,3 +55,4 @@ __________
 `);
 
 game("javascript", []);
+
